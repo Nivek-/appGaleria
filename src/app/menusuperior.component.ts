5472bafcc6aa9d6d321580c46a5c132a640666b3
Kgutierrez <Nivek-@users.noreply.github.com>
2020-04-06 23:25:55 -0400

    Commit Inicial
    Commit Inicial

---
+++ src/app/menusuperior.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'unab-menusuperior',
+  templateUrl: './menusuperior.component.html',
+  styles: ['h1 { font-weight: normal; }']
+})
+export class MenusuperiorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
