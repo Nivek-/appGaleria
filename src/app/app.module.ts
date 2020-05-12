b84d4a0e28e923bd70b8935536ec08493c17675b
Kgutierrez <Nivek-@users.noreply.github.com>
2020-05-12 00:07:19 -0400

    Integracion API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { ImagenService } from "./services/imagen.service";
+import { HttpModule } from "@angular/http";
 
 import { routes } from "./app-routing.module";
 import { AppComponent } from "./app.component";
